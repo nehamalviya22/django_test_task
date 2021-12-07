af184fae6a9d29a493c98577a8649188ed0c9a8a
User29 <you@example.com>
2021-12-07 13:17:52 +0530

    create get impression api

--- advertisement/advertise_app/urls.py
+++ advertisement/advertise_app/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import GetAd
+from .views import GetAd,Impression
 
 urlpatterns = [
     path('GetAd/', GetAd.as_view()),
+    path('Impression/', Impression.as_view()),
 ]
--- advertisement/advertise_app/views.py
+++ advertisement/advertise_app/views.py
@@ -18,7 +18,7 @@ class GetAd(APIView):
             return Response({"message": "Please provide all following parameters SDK Version,SessionId,Platform,User name,Country code"},status = 400)
 
 
-        user, created = username.objects.get_or_create(username=user_name)
+        user, created = Username.objects.get_or_create(username=user_name)
         if created:
             user.ad_count = 1
             user.save()
@@ -26,7 +26,7 @@ class GetAd(APIView):
             user.ad_count = user.ad_count+1
             user.save()
         
-        sdk, created = sdkversion.objects.get_or_create(sdkversion=SDK_version)
+        sdk, created = Sdkversion.objects.get_or_create(sdkversion=SDK_version)
         if created:
             sdk.ad_count = 1
             sdk.save()
@@ -46,3 +46,31 @@ class GetAd(APIView):
 
         return Response(r.text,content_type="text/xml; charset=utf-8")
 
+class Impression(APIView):
+    def get(self, request):
+        SDK_version = request.query_params.get("SDK Version")
+        sessionid = request.query_params.get("SessionId")
+        platform = request.query_params.get("Platform")
+        user_name = request.query_params.get("User name")
+        country_code = request.query_params.get("Country code")
+
+        if not SDK_version or not sessionid or not platform or not user_name or not country_code:
+            return Response({"message": "Please provide all following parameters SDK Version,SessionId,Platform,User name,Country code"},status = 400)
+
+        user, created = Username.objects.get_or_create(username=user_name)
+        if created:
+            user.impression_count = 1
+            user.save()
+        else:
+            user.impression_count = user.impression_count+1
+            user.save()
+        
+        sdk, created = Sdkversion.objects.get_or_create(sdkversion=SDK_version)
+        if created:
+            sdk.impression_count = 1
+            sdk.save()
+        else:
+            sdk.impression_count = sdk.impression_count+1
+            sdk.save()
+
+        return Response(status = 200)
